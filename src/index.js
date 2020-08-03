a45d7d70174a34b3117905e2119462c627925ac0
hashntrash <oliverchong987@gmail.com>
2020-08-03 10:03:27 +0800

    added dev and production configs, set up dev server

--- src/index.js
+++ src/index.js
@@ -4,5 +4,4 @@ import { AlertService } from "./app/alert.service";
 import { ComponentService } from "./app/component.service";
 const alertService = new AlertService();
 const componentService = new ComponentService();
-console.log('hi');
 run(alertService, componentService);
